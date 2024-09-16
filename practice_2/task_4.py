c76bc82b21f39afe9642eea5bbfa7804c1f20927
AleksandrMaks <you@example.com>
2024-09-16 19:49:46 +0300

    done 4 task

--- practice_2/task_4.py
+++ practice_2/task_4.py
@@ -4,3 +4,17 @@ Develop a program that asks the user for an integer 4-digit number and calculate
 3 + 1 + 4 + 1 = 9
 
 """
+
+def checkNumber(number):
+    if number.isdigit():
+        b = int(number)
+        digits = set(number)
+        return (1000 < b < 9999) and len(digits) == 4
+    else:
+        return False
+x = int(input())
+y = int(input())
+z = int(input())
+v = int(input())
+b = x+y+z+v
+print(x, '+', y, '+', z, '+', v, '=', b)
